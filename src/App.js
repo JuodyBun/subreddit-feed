dbe425e7da1a8dc15152bcd3687e4735cfca2ca4
Juody Bun <bjuody@gmail.com>
2020-12-19 13:43:42 -0600

    Creating states and effects

--- src/App.js
+++ src/App.js
@@ -1,11 +1,16 @@
-import React from 'react';
+import React, {useState, useEffect} from 'react';
 
 function App() {
+  const [articles, setArticles] = useState([]); //use state as an empty array
+
   return (
     <div className="App">
       <header className="App-header">
-        Header
+        <input type="text" className="input" value="webdev"/>
       </header>
+      <div className="articles">
+
+      </div>
     </div>
   );
 }     
